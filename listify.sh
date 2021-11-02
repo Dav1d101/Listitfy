beb06aae82bff5b6634383f92dfa18a15b7be000
David Iljuchin <david.iljuchin@gmail.com>
2021-11-02 09:26:12 +0100

    better listify

--- listify.sh
+++ listify.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-LIST_DEFAULT="-"
-LIST_CHOICE=$1
-TEXT=$2
-if [ "$LIST_CHOICE" == "" ]
-then
-	LIST=$LIST_DEFAULT
-else
-	LIST=$LIST_CHOICE
-fi
+LIST=${1:--}
 sed s/^/$LIST/ $TEXT
 
 
