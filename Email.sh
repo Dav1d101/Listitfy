d02203b47910aea2b3f45502389795550de41376
David Iljuchin <david.iljuchin@gmail.com>
2021-11-04 13:22:55 +0100

    sending email with nc

---
+++ Email.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+RECEIVER=$1
+SUBJECT=$2
+MESSAGE=$3
+
+{
+echo "HELO Saturn"
+echo "MAIL FROM:<david.iljuchin@gmail.com>"
+echo "RCPT TO:<$RECEIVER>"
+echo "DATA"
+echo "From:<david.iljuchin@gmail.com>"
+echo "To:<$RECEIVER>"
+echo "Subject: $SUBJECT"
+echo "$MESSAGE"
+echo "."
+echo "QUIT"
+} | nc localhost 25
